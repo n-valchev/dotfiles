2f7dee3a42743ad7f5a274a6312f07dbed722137
Nikolay Valchev <nivalchev@gmail.com>
2025-12-10 22:47:10 +0200

    tmux powerlines

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ tmux git ];
+{pkgs, ...}: {
+  home.packages = with pkgs; [tmux git];
 
   home.file.".tmux.conf".source = ./tmux.conf;
+  home.file."tmux-powerline/config.sh".source = ./powerlines.sh;
+  home.file."tmux-powerline/themes/custom.sh".source = ./theme.sh;
 }
