50252db8a41177bcd29a876f4026f606768304db
Nikolay Valchev <nivalchev@gmail.com>
2025-12-14 23:02:52 +0200

    kitty fonts

--- config/fonts.nix
+++ config/fonts.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }:
+{pkgs, ...}:
 with pkgs; [
   nerd-fonts.fira-code
   nerd-fonts.fira-mono
   nerd-fonts.hack
   nerd-fonts.noto
   nerd-fonts.ubuntu
+  fira-code
+  fira-code-symbols
 ]
