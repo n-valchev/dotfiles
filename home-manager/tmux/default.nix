06f028b5890c1a5e29df92afbc46b86f4e6f73b8
Nikolay Valchev <nikolay.valchev@redis.com>
2025-12-16 09:49:53 +0200

    Use tmux Symlink & cmd+X kill-window

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -1,9 +1,7 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [tmux git];
+{mkDotfilesSymlink, ...}: {
+  home.file.".tmux.conf".source = mkDotfilesSymlink "tmux/tmux.conf";
 
-  home.file.".tmux.conf".source = ./tmux.conf;
+  xdg.configFile."tmux-powerline/config.sh".source = mkDotfilesSymlink "tmux/powerlines.sh";
 
-  xdg.configFile."tmux-powerline/config.sh".source = ./powerlines.sh;
-
-  xdg.configFile."tmux-powerline/themes/custom.sh".source = ./theme.sh;
+  xdg.configFile."tmux-powerline/themes/custom.sh".source = mkDotfilesSymlink "tmux/theme.sh";
 }
